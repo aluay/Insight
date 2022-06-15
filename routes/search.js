2bfa38053ae56217dc682c156c65651aaa82943f
Abdullah Luay <aluay95@gmail.com>
2022-06-15 01:12:10 -0700

    Added some styling for the homepage

--- routes/search.js
+++ routes/search.js
@@ -8,6 +8,12 @@ router.get("/:keyword", async (req, res) => {
     res.send(result);
 });
 
+/*
+
+    REMOVE OPTIONS AND OTHER STUFF FROM RESULTS
+    *****ONLY RETURN STOCKS****
+
+*/
 //   Search for all matches based on keyword
 async function search(keyword) {
     const URL = `https://query2.finance.yahoo.com/v1/finance/search?q=${keyword}&lang=en-US&region=US&quotesCount=6&newsCount=2&listsCount=2&enableFuzzyQuery=false&quotesQueryId=tss_match_phrase_query&multiQuoteQueryId=multi_quote_single_token_query&newsQueryId=news_cie_vespa&enableCb=true&enableNavLinks=true&enableEnhancedTrivialQuery=true`;
